90f3db3a28ac0ba5d3018a2a9fce7baa9fa65455
Max Marschhauser <max.marschhauser@gmail.com>
2023-05-31 10:39:19 +0200

    added question screen and data

---
+++ lib/answer_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  // AnswerButton(this.answerText, this.onTap, {super.key}); // props su pozicionalni argumenti
+  const AnswerButton(
+      {super.key,
+      required this.answerText,
+      required this.onTap}); // props su named argumenti, razlika je da im moram staviti required jer inače named argumenti nisu obavezni
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          backgroundColor: const Color.fromARGB(255, 28, 4, 85),
+          foregroundColor: Colors.white, // text color
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
+        child: Text(answerText));
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/answer_button.dart';
+import 'package:flutter_quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +14,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions Screen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double
+          .infinity, // iskoristi maksimalnu širinu, koliko god ima na ekranu
+      child: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.center, // space between, space evenly... kao flex
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((answer) {
+            // tri točke su spread operator
+            return AnswerButton(
+              answerText: answer,
+              onTap: () {},
+            ); // ovako bih importao kad su named argumenti, tada nije bitan redosljed navođenja argumenata
+            //  AnswerButton('Answer text...', () {}), // ovako bih importao kad su pozicionalni argumenti
+          })
+        ],
+      ),
+    );
   }
 }
